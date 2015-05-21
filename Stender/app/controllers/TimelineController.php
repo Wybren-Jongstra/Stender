2e40c926ed15b82c5817b6f5e9f2a13bf735a5cf
Wybren_SC <wybren@puuridee.nl>
2015-05-21 15:10:23 +0200

    filling sessions

--- Stender/app/controllers/TimelineController.php
+++ Stender/app/controllers/TimelineController.php
@@ -4,7 +4,16 @@ class TimelineController extends BaseController {
 
     public function getTimeline()
     {
+        $this->fillSession();
         return View::make('timeline');
+    }
+
+    public static function fillSession()
+    {
+        Session::put('UserID', Auth::user()->UserID);
+        Session::put('UserKindID', Auth::user()->UserKindID);
+        Session::put('UserProfileID', Auth::user()->UserProfileID);
+        Session::put('Email', Auth::user()->Email);
     }
 }
 
