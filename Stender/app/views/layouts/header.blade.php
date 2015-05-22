38d9341f207130c8048b9e284f3c89104406cc25
Markr10 <mark-r@live.nl>
2015-05-22 22:49:27 +0200

    Some cleanup - Full path to resources - HTML5 DOCYPE - Some HTML5 cleanup

--- Stender/app/views/layouts/header.blade.php
+++ Stender/app/views/layouts/header.blade.php
@@ -1,21 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//NL" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
     <head>
         <title>Stender home</title>
 
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+        <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+        <link rel="icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="{{ URL::to('css/style.css') }}" media="screen" />
 @yield('custom-stylesheets')
-        <link type="text/css" href="css/bootstrap.css" rel="stylesheet" media="screen, projection" />
-        <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="screen, projection" />
+        <link type="text/css" href="{{ URL::to('css/bootstrap.css') }}" rel="stylesheet" media="screen, projection" />
+        <link type="text/css" href="{{ URL::to('css/bootstrap.min.css') }}" rel="stylesheet" media="screen, projection" />
 
-        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script src="{{ URL::to('js/jquery.min.js') }}"></script>
 @yield('jquery-scripts')
-        <script type="text/javascript" src="js/bootstrap.js"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script src="{{ URL::to('js/bootstrap.js') }}"></script>
+        <script src="{{ URL::to('js/bootstrap.min.js') }}"></script>
 
 @yield('custom-scripts')
     </head>
