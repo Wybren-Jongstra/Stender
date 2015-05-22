07665a5ee771d0c675844833e01451a7839f5e39
Markr10 <mark-r@live.nl>
2015-05-22 10:10:38 +0200

    Attribute names fixed

--- Stender/app/models/User.php
+++ Stender/app/models/User.php
@@ -34,14 +34,14 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
     *
     * @var array
     */
-    protected $guarded = array('id');
+    protected $guarded = array('UserID');
 	
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
+	protected $hidden = array('Password', 'RememberToken');
 
     // public function getAuthIdentifier()
     // {
