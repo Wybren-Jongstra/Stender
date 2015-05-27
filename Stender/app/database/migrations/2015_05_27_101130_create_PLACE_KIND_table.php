f27cb48c5bb0e798c508efeb9d835a0b8033805f
Markr10 <mark-r@live.nl>
2015-05-27 15:39:25 +0200

    New migration files added, drop tables migration file added

---
+++ Stender/app/database/migrations/2015_05_27_101130_create_PLACE_KIND_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePLACEKINDTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('PLACE_KIND', function(Blueprint $table)
+		{
+			$table->integer('PlaceKindID', true);
+			$table->string('Name', 20)->unique('Name_UNIQUE');
+			$table->string('Description', 100)->unique('Description_UNIQUE');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('PLACE_KIND');
+	}
+
+}
