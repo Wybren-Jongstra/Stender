59a001db4e0e6f1124ee033404dc184a2e5388fc
Markr10 <mark-r@live.nl>
2015-06-01 18:24:02 +0200

    Down function implemented, code reformatted for better object-oriented design

--- Stender/app/database/migrations/2015_05_27_100000_drop_tables.php
+++ Stender/app/database/migrations/2015_05_27_100000_drop_tables.php
@@ -1,37 +1,16 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use App\Database\Migrations\AbstractDropTable;
 
 /**
  * Class DropTables
  * Drops the tables of the previous database version.
  */
-class DropTables extends Migration {
+class DropTables extends AbstractDropTable {
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     * @throws Exception
-     */
-	public function up()
-	{
-        // Check if there are no app tables in the database so that
-        // the up method deactivates automatically without breaking php artisan migrate.
-        // Use the default auth table for that check.
-        // Throw an error when this check is not possible. So when the configuration for the auth table is not found.
-        if(is_null(Config::get('auth.table')))
-        {
-            throw new Exception('Configuration for auth table not found!');
-        }
-        elseif(! Schema::hasTable(Config::get('auth.table')))
-        {
-            return;
-        }
-
-        $migrationObjects = array
-        (
+    protected function getMigrationObjectsArray()
+    {
+        return array(
             new CreateCONNECTIONKINDTable(),
             new CreateCONNECTIONSTATUSTable(),
             new CreateCONNECTIONTable(),
@@ -69,23 +48,6 @@ class DropTables extends Migration {
             new AddForeignKeysToUSERVOTETable(),
             new AddForeignKeysToUSERTable(),
         );
-
-        // Loop backwards because the foreign keys must first be removed.
-        for($i = (count($migrationObjects) - 1); $i >= 0; $i--)
-        {
-            // down method rolls back the migration file
-            $migrationObjects[$i]->down();
-        }
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    }
 
 }
