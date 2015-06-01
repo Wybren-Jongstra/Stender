06495daf60212a4a0aa833692a407ee37e4716d5
Markr10 <mark-r@live.nl>
2015-06-01 15:01:51 +0200

    Migration files updated

---
+++ Stender/app/database/migrations/2015_06_01_125444_create_HASHTAG_KIND_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateHASHTAGKINDTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('HASHTAG_KIND', function(Blueprint $table)
+		{
+			$table->integer('HashtagKindID', true);
+			$table->string('Name', 20)->unique('Name_UNIQUE');
+			$table->string('Description', 100);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('HASHTAG_KIND');
+	}
+
+}
---
+++ Stender/app/database/migrations/2015_06_01_125444_create_SKILL_KIND_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateSKILLKINDTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('SKILL_KIND', function(Blueprint $table)
+		{
+			$table->integer('SkillKindID', true);
+			$table->string('Name', 20)->unique('Name_UNIQUE');
+			$table->string('Description', 100);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('SKILL_KIND');
+	}
+
+}
