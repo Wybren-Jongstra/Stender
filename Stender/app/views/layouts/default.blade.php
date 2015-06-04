e4b68b8e1ebe35354fa86504e7d2c8b0edfdc565
Markr10 <mark-r@live.nl>
2015-06-04 21:25:36 +0200

    Title added as section

--- Stender/app/views/layouts/default.blade.php
+++ Stender/app/views/layouts/default.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.header')
+@section('title', 'home')
 @section('body-attributes')id="body-home"@endsection
 @section('body')
     @yield('content')
--- Stender/app/views/layouts/header.blade.php
+++ Stender/app/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
     <head>
-        <title>Stender home</title>
+        <title>Stender @yield('title')</title>
 
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
