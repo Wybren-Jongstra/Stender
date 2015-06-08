6e4f6f9a60ee4c36ad2d476097d7b84d96b06f7d
Markr10 <mark-r@live.nl>
2015-06-08 13:56:16 +0200

    Migration files and increased number for ERD V1.14

---
+++ Stender/app/database/migrations/2015_06_08_114600_add_foreign_keys_to_HASHTAG_table6.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToHASHTAGTable6 extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('HASHTAG', function(Blueprint $table)
+		{
+			$table->foreign('UserProfileID', 'fk_SKILL_USER_PROFILE10')->references('UserProfileID')->on('USER_PROFILE')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+			$table->foreign('AccountKindID', 'fk_HASHTAG_ACCOUNT_KIND1')->references('AccountKindID')->on('ACCOUNT_KIND')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('HASHTAG', function(Blueprint $table)
+		{
+			$table->dropForeign('fk_SKILL_USER_PROFILE10');
+			$table->dropForeign('fk_HASHTAG_ACCOUNT_KIND1');
+		});
+	}
+
+}
