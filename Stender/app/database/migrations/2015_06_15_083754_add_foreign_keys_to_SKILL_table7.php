06aec2e840ad3395950ca6b05c9477249d7371a6
Markr10 <mark-r@live.nl>
2015-06-15 10:55:54 +0200

    Migration files and increased number for ERD V1.15

---
+++ Stender/app/database/migrations/2015_06_15_083754_add_foreign_keys_to_SKILL_table7.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToSKILLTable7 extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('SKILL', function(Blueprint $table)
+		{
+			$table->foreign('AccountKindID', 'fk_SKILL_ACCOUNT_KIND1')->references('AccountKindID')->on('ACCOUNT_KIND')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+			$table->foreign('UserProfileID', 'fk_SKILL_USER_PROFILE1')->references('UserProfileID')->on('USER_PROFILE')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('SKILL', function(Blueprint $table)
+		{
+			$table->dropForeign('fk_SKILL_ACCOUNT_KIND1');
+			$table->dropForeign('fk_SKILL_USER_PROFILE1');
+		});
+	}
+
+}
