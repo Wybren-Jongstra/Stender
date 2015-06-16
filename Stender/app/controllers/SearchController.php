2df80c8f3151f58b88473bbd46c249ee7d8ea590
Markr10 <mark-r@live.nl>
2015-06-16 13:39:45 +0200

    Subquery removed

--- Stender/app/controllers/SearchController.php
+++ Stender/app/controllers/SearchController.php
@@ -9,26 +9,21 @@ class SearchController extends BaseController {
 	 */
     public function autocomplete()
     {
-        $search = User::select('UserID', 'UserProfileID')
-            ->whereIn('UserProfileID', function($query)
-            {
-                $term = Input::get('term');
+        $term = Input::get('term');
 
-                $query->select('UserProfileID')
-                    ->from((new UserProfile)->getTable())  // Creates a new model to grab the table name
-                    ->where('FirstName', 'LIKE', '%'.$term.'%')
-                    ->orWhere('Surname', 'LIKE', '%'.$term.'%')
-                    ->orWhere('DisplayName', 'LIKE', '%'.$term.'%');
-            })
-            ->orderBy('UserID')
+        $search = UserProfile::where('FirstName', 'LIKE', '%'.$term.'%')
+            ->orWhere('Surname', 'LIKE', '%'.$term.'%')
+            ->orWhere('DisplayName', 'LIKE', '%'.$term.'%')
+            ->orderBy('UserProfileID')
+            ->select('UserProfileID', 'DisplayName')
             ->take(5)
-            ->with('userProfile') // Start with lower case to to indicate that it is an added item and no table column.
+            ->with('user') // Start with lower case to to indicate that it is an added item and no table column.
             ->get();
 
         $results = array();
         foreach ($search as $row)
         {
-            $results[] = ['id' => $row->UserID, 'label' => $row->userProfile->DisplayName, 'actor'=> $row->userProfile->DisplayName];
+            $results[] = ['id' => $row->user->UserID, 'label' => $row->DisplayName, 'actor'=> $row->DisplayName];
         }
 
         return Response::json($results);
