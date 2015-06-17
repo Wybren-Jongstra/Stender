dba2b15a3d0072762019f8c9e2b09d9740ea3dd7
Markr10 <mark-r@live.nl>
2015-06-17 14:40:15 +0200

    Pop-up will now close automatically

--- Stender/app/views/closePopup.blade.php
+++ Stender/app/views/closePopup.blade.php
@@ -1,9 +1,17 @@
-@extends('layouts.common')
-@section('custom-jquery')
-    $this.close();
-@endsection
-@section('content')
-    <div id="content" class="container">
-        
-    </div>
+@extends('layouts.simpleHeader')
+@section('custom-scripts')
+    <script>
+        /**
+         * Refresh the parent window
+         */
+        function refreshParent()
+        {
+            window.opener.location.reload();
+        }
+
+        // Reload parent on unload
+        window.onunload = refreshParent;
+        // Close window
+        window.close();
+    </script>
 @stop
---
+++ Stender/app/views/layouts/simpleHeader.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+        <link rel="icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+        @yield('custom-scripts')
+    </head>
+</html>
