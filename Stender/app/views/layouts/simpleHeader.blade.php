c5c21e556d73fcb4d94cd2481c3f80e24802571c
Markr10 <mark-r@live.nl>
2015-06-18 02:05:48 +0200

    Updated layout

--- Stender/app/views/layouts/simpleHeader.blade.php
+++ Stender/app/views/layouts/simpleHeader.blade.php
@@ -4,6 +4,8 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
         <link rel="icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+        {{ HTML::style('css/style.css', array('media' => 'screen, projection')) }}
+        {{ HTML::style('packages/bootstrap/css/bootstrap.css', array('media' => 'screen, projection')) }}
         @yield('custom-scripts')
     </head>
     <body>
