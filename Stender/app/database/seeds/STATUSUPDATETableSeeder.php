8d7acd28ee11b805338f7bb2b819c85268c1ab87
Markr10 <mark-r@live.nl>
2015-06-30 16:29:34 +0200

    Fix displaying new line characters

--- Stender/app/database/seeds/STATUSUPDATETableSeeder.php
+++ Stender/app/database/seeds/STATUSUPDATETableSeeder.php
@@ -7,7 +7,7 @@ class STATUSUPDATETableSeeder extends Seeder {
         $dbEntries = array(
             array('UserID' => '4', 'DateCreated' => '2015-06-18 08:07:57', 'Deleted' => '0', 'Text' => 'Lekker piano gespeeld =D', 'ImageUrlPart' => 'uploads/test_pictures/4_Fg9vIO_message_picture_mark.jpg'),
             array('UserID' => '2', 'DateCreated' => '2015-06-18 08:35:12', 'Deleted' => '0', 'Text' => 'Druk aan het testen en het voorbereiden op de presentatie', 'ImageUrlPart' => NULL),
-            array('UserID' => '1', 'DateCreated' => '2015-06-18 08:36:58', 'Deleted' => '0', 'Text' => 'Geweldige actie van Mamma Mia... 2 Pizza\'s halen 1 betalen!!!!!!\r\nAlleen Maandag 32 Juni!', 'ImageUrlPart' => NULL),
+            array('UserID' => '1', 'DateCreated' => '2015-06-18 08:36:58', 'Deleted' => '0', 'Text' => "Geweldige actie van Mamma Mia... 2 Pizza\'s halen 1 betalen!!!!!!\r\nAlleen Maandag 32 Juni!", 'ImageUrlPart' => NULL),
         );
 
         DB::table('STATUS_UPDATE')->insert($dbEntries);
